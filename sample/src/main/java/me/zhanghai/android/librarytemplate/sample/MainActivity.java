5c2ed7952d128a9653a28b0c4daeb59a070e7d94
Hai Zhang <dreaming.in.code.zh@gmail.com>
2019-09-27 19:13:38 -0700

    [Feature] Initial commit.

---
+++ sample/src/main/java/me/zhanghai/android/librarytemplate/sample/MainActivity.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019 Hai Zhang <dreaming.in.code.zh@gmail.com>
+ * All Rights Reserved.
+ */
+
+package me.zhanghai.android.librarytemplate.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
